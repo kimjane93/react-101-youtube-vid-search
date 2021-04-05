1e9dd3d93ae2cf2dfa853415c7f315fd71b29093
kimjane93 <kjchadwell93@gmail.com>
2021-04-05 09:22:57 -0400

    retrieve new key and install dotenv

--- src/api/youtubeApi.jsx
+++ src/api/youtubeApi.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const KEY = 'AIzaSyCghlBYPCNibVNuBgeTF5H2r4-brQM0UUc'
+const KEY = process.env.REACT_APP_API_KEY
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
